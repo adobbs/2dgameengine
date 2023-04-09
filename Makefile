01c105adddd0ef2e8fd0740194a107775aacb3f1
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-09 14:28:02 -0400

    Add dependencies

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 build:
-	g++ -Wall -std=c++17 src/*.cpp -lSDL2 -o gameengine
+	g++ -Wall -std=c++17 -I"./libs/" src/*.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -llua -o gameengine
 
 run:
 	./gameengine
 
 clean:
-	rm gameengine
+	rm ./gameengine
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main()
 {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
     SDL_Init(SDL_INIT_EVERYTHING);
+
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
