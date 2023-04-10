c981e3d4e23e6cffc90b845ab9fd821a7ca9dbcf
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-10 15:06:38 -0400

    Implement fake fullscreen window

--- src/Game.cpp
+++ src/Game.cpp
@@ -20,12 +20,16 @@ void Game::Initialize()
         std::cerr << "Error initializing SDL." << std::endl;
         return;
     }
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    windowWidth = displayMode.w;
+    windowHeight = displayMode.h;
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        windowWidth,
+        windowHeight,
         SDL_WINDOW_BORDERLESS);
     if (!window)
     {
