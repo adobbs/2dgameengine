86fe0c4eb608a7971a319e9f7fbac9d4ac98e2d1
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-12 16:45:17 -0400

    Create player position and velocity

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 Game::Game()
@@ -50,6 +51,7 @@ void Game::Initialize()
 
 void Game::Run()
 {
+    Setup();
     while (isRunning)
     {
         ProcessInput();
@@ -78,14 +80,19 @@ void Game::ProcessInput()
     }
 }
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
 void Game::Setup()
 {
-    // TODO: Initialize game objects...
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(1.0, 1.0);
 }
 
 void Game::Update()
 {
-    // TODO: Update game objects
+    playerPosition.x += playerVelocity.x;
+    playerPosition.y += playerVelocity.y;
 }
 
 void Game::Render()
@@ -98,7 +105,11 @@ void Game::Render()
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
-    SDL_Rect dstRect = {10, 10, 32, 32};
+    SDL_Rect dstRect = {
+        static_cast<int>(playerPosition.x),
+        static_cast<int>(playerPosition.y),
+        32,
+        32};
 
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
 
