d68288765c66befd81a8aa860faaa3bd1be20e5c
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-14 16:25:45 -0400

    Multiply by deltaTime and convert speed

--- src/Game.cpp
+++ src/Game.cpp
@@ -55,7 +55,7 @@ glm::vec2 playerVelocity;
 void Game::Setup()
 {
     playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(0.5, 0.0);
+    playerVelocity = glm::vec2(100.0, 0.0);
 }
 
 void Game::ProcessInput()
@@ -86,11 +86,13 @@ void Game::Update()
         SDL_Delay(timeToWait);   
     }
 
+    double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+
     // Store the current frame time
     millisecsPreviousFrame = SDL_GetTicks();
 
-    playerPosition.x += playerVelocity.x;
-    playerPosition.y += playerVelocity.y;
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render()
