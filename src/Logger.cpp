2339c80f75f33ff2b229d8b13ff57728861e5c09
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-15 19:17:34 -0400

    Create LogEntry, LogType, and refactor logging

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,19 +2,35 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+#include <chrono>
+#include <string>
 
 #define ESC "\033["
 #define GREEN_TEXT "32m"
 #define RED_TEXT "31m"
 #define RESET "\033[m"
 
-auto t = std::time(nullptr);
-auto tm = *std::localtime(&t);
+std::vector<LogEntry> Logger::messages;
+
+std::string CurrentDateTimeToString() {
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string output(30, '\0');
+    std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S", std::localtime(&now));
+    return output;
+}
 
 void Logger::Log(const std::string& message) {
-    std::cout << ESC << GREEN_TEXT << "LOG | " << std::put_time(&tm, "%d-%b-%Y %H:%M:%S") << " - " << message << RESET << std::endl;
+    LogEntry logEntry;
+    logEntry.type = LOG_INFO;
+    logEntry.message = "LOG | " + CurrentDateTimeToString() + " - " + message; 
+    std::cout << ESC << GREEN_TEXT << logEntry.message << RESET << std::endl;
+    messages.push_back(logEntry);
 }
 
 void Logger::Err(const std::string& message) {
-    std::cout << ESC << RED_TEXT << "ERR | " << std::put_time(&tm, "%d-%b-%Y %H:%M:%S") << " - " << message << RESET << std::endl;
+    LogEntry logEntry;
+    logEntry.type = LOG_ERROR;
+    logEntry.message = "ERR | " + CurrentDateTimeToString() + " - " + message;
+    std::cout << ESC << RED_TEXT << logEntry.message << RESET << std::endl;
+    messages.push_back(logEntry);
 }
--- src/Logger.h
+++ src/Logger.h
@@ -2,12 +2,24 @@
 #define LOGGER_H
 
 #include <string>
+#include <vector>
 
-class Logger
-{
-public:
-    static void Log(const std::string& message);
-    static void Err(const std::string& message);
+enum LogType {
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+};
+
+struct LogEntry {
+    LogType type;
+    std::string message;
+};
+
+class Logger {
+    public:
+        static std::vector<LogEntry> messages;
+        static void Log(const std::string& message);
+        static void Err(const std::string& message);
 };
 
 #endif
