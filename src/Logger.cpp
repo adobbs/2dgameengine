2f0f253f992de29b0e977e168ebdf5a335a635cd
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-15 06:46:06 -0400

    Add date and time stamp to Logger

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,15 +1,20 @@
 #include "Logger.h"
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 #define ESC "\033["
 #define GREEN_TEXT "32m"
 #define RED_TEXT "31m"
 #define RESET "\033[m"
 
+auto t = std::time(nullptr);
+auto tm = *std::localtime(&t);
+
 void Logger::Log(const std::string& message) {
-    std::cout << ESC << GREEN_TEXT << "LOG: " << message << RESET << std::endl;
+    std::cout << ESC << GREEN_TEXT << "LOG: [ " << std::put_time(&tm, "%d/%b/%Y %H:%M:%S") << " ] " << message << RESET << std::endl;
 }
 
 void Logger::Err(const std::string& message) {
-    std::cout << ESC << RED_TEXT << "ERROR: " << message << RESET << std::endl;
+    std::cout << ESC << RED_TEXT << "ERROR: [ " << std::put_time(&tm, "%d/%b/%Y %H:%M:%S") << " ] " << message << RESET << std::endl;
 }
