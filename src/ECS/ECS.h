27d1c4a46d8ba612efb872f1e464ee0e4cd4c2bd
Andy Dobbs <andrew.dobbs@gmail.com>
2023-04-20 10:44:39 -0400

    Create Registry system management methods

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -150,19 +150,16 @@ class Registry {
         template <typename TComponent, typename ...TArgs> void AddComponent(Entity entity, TArgs&& ...args);
         template <typename TComponent> void RemoveComponent(Entity entity);
         template <typename TComponent> bool HasComponent(Entity entity) const;
+
+        template <typename TSystem, typename ...TArgs> void AddSystem(TArgs && ...args);
+        template <typename TSystem> void RemoveSystem();
+        template <typename TSystem> bool HasSystem() const;
+        template <typename TSystem> TSystem& GetSystem() const;
         
         // TODO:
         // KillEntity()
 
-        // AddComponent(Entity entity)
-        // RemoveComponent(Entity entity)
-        // HasComponent(Entity entity)
         // GetComponent(Entity entity)
-
-        // AddSystem()
-        // RemoveSystem()
-        // HasSystem()
-        // GetSystem()
 };
 
 template <typename TComponent>
@@ -209,6 +206,29 @@ bool Registry::HasComponent(Entity entity) const {
     const auto componentId = Component<TComponent>::GetId();
     const auto entityId = entity.GetId();
     return entityComponentSignatures[entityId].test(componentId);
+}
+
+template <typename TSystem, typename ...TArgs>
+void Registry::AddSystem(TArgs&& ...args) {
+    TSystem* newSystem(new TSystem(std::forward<TArgs>(args)...));
+    systems.insert(std::make_pair(std::type_index(typeid(TSystem)), newSystem));
+}
+
+template <typename TSystem>
+void Registry::RemoveSystem() {
+    auto system = systems.find(std::type_index(typeid(TSystem)));
+    systems.erase(system);
+}
+
+template <typename TSystem>
+bool Registry::HasSystem() const {
+    return systems.find(std::type_index(typeid(TSystem))) != systems.end();
+}
+
+template <typename TSystem>
+TSystem& Registry::GetSystem() const {
+    auto system = systems.find(std::type_index(typeid(TSystem)));
+    return *(std::static_pointer_cast<TSystem>(system->second));
 }
 
 #endif
